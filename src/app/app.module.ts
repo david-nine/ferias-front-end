fce52071d1b6353a54f6e69180c2cd3af037b84f
david-nine <davihildebran@gmail.com>
2021-06-08 08:49:39 -0300

    renomecao componente requerimento-colaborador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RequerimentoColaboradorComponent } from './requerimento-colaborador/requerimento-colaborador.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RequerimentoColaboradorComponent,
+    FooterComponent
   ],
   imports: [
+    RouterModule.forRoot([
+      { path: '', component: RequerimentoColaboradorComponent },
+    ]),
     BrowserModule,
-    AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
-})
+})  
+
 export class AppModule { }
---
+++ src/app/requerimento-colaborador/requerimento-colaborador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'requerimento-colaborador-page',
+  templateUrl: './requerimento-colaborador.component.html',
+  styleUrls: ['./requerimento-colaborador.component.css']
+})
+export class RequerimentoColaboradorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
