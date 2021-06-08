a07b3ef349c4fc3996f9f83565d2207aec3cbc82
Leonardo F Silva <felipeleao217@gmail.com>
2021-06-08 17:41:49 -0300

    OLOCO MEU, alteração no seletor de data na tela do gesto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-// import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RequerimentoColaboradorComponent } from './requerimento-colaborador/requerimento-colaborador.component';
 import { FooterComponent } from './footer/footer.component';
+import { AvaliacaoGestorComponent } from './avaliacao-gestor/avaliacao-gestor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     RequerimentoColaboradorComponent,
-    FooterComponent
+    FooterComponent,
+    AvaliacaoGestorComponent
   ],
   imports: [
     RouterModule.forRoot([
       { path: '', component: RequerimentoColaboradorComponent },
+      { path: 'gestor', component: AvaliacaoGestorComponent },
     ]),
     BrowserModule,
   ],
   bootstrap: [AppComponent]
-})  
+})
 
 export class AppModule { }
--- src/app/avaliacao-gestor/avaliacao-gestor.component.ts
+++ src/app/avaliacao-gestor/avaliacao-gestor.component.ts
@@ -11,5 +11,4 @@ export class AvaliacaoGestorComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
