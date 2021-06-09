f2f37ed46d206771a681d86330bf0b4d6b81462b
Tharlys Dias <tharlys.dias@outlook.com>
2021-06-09 11:14:46 -0300

    Criacao dos metodos para buscar requerimentos

---
+++ src/app/requerimento.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Requerimento } from './requerimento';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RequerimentoService {
+
+  constructor(private http: HttpClient) { }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error); // log to console instead
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  buscarTodosOsRequerimentos(): Observable<Requerimento[]> {
+    const url = 'http://localhost:8080/requerimento';
+    return this.http
+      .get<Requerimento[]>(url)
+      .pipe(catchError(this.handleError<Requerimento[]>('buscarTodosOsRequerimentos', [])));
+  }
+
+  buscarRequerimentoPorId(idRequerimento: number): Observable<Requerimento> {
+    const url = `http://localhost:8080/requerimento/${idRequerimento}`;
+    return this.http
+      .get<Requerimento>(url)
+      .pipe(
+        catchError(
+          this.handleError<Requerimento>(`buscarRequerimentoPorId idRequerimento=${idRequerimento}`)
+        )
+      );
+  }
+
+  buscarTodosOsRequerimentosPorIdColaborador(
+    idColaborador: number
+  ): Observable<Requerimento[]> {
+    const url = `http://localhost:8080/requerimento/colaborador/${idColaborador}`;
+    return this.http
+      .get<Requerimento[]>(url)
+      .pipe(
+        catchError(
+          this.handleError<Requerimento[]>(
+            'buscarTodosOsRequerimentosPorIdColaborador idColaborador=${idColaborador}',
+            []
+          )
+        )
+      );
+  }
+
+  buscarRequerimentosPorIdEEstadoColaborador(
+    idColaborador: number, estado: string
+  ): Observable<Requerimento[]> {
+    const url = `http://localhost:8080/requerimento/colaborador/${idColaborador}/estado`;
+    return this.http
+      .get<Requerimento[]>(url, estado)
+      .pipe(
+        catchError(
+          this.handleError<Requerimento[]>(
+            'buscarRequerimentosPorIdEEstadoColaborador idColaborador=${idColaborador} estado',
+            []
+          )
+        )
+      );
+  }
+}
