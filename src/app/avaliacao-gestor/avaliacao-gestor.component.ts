846403349bc125361897673d898f385420d92ecb
Leonardo F Silva <felipeleao217@gmail.com>
2021-06-09 16:54:50 -0300

    AvaliacaoGestor consumindo a API e mostrando na web page

--- src/app/avaliacao-gestor/avaliacao-gestor.component.ts
+++ src/app/avaliacao-gestor/avaliacao-gestor.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { RequerimentoService } from './../requerimento.service';
+import { SaldoService } from './../saldo.service';
+
+import { Requerimento } from '../requerimento';
+import { Saldo } from './../saldo';
+
+
 @Component({
   selector: 'app-avaliacao-gestor',
   templateUrl: './avaliacao-gestor.component.html',
@@ -7,8 +14,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AvaliacaoGestorComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private requerimentoService: RequerimentoService,
+    private saldoService: SaldoService
+    ) { }
+
+  requerimentos: Requerimento[] = [];
+  requerimentosPorData: Requerimento[] = [];
 
   ngOnInit(): void {
+    this.buscarTodosRequerimentos();
+  }
+
+  buscarTodosRequerimentos(): void {
+    this.requerimentoService.buscarTodosOsRequerimentos()
+    .subscribe((requerimento) => (this.requerimentos = requerimento));
   }
 }
