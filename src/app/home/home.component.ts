7e4f42e4d01524f1cb556440adc59c54120549f4
Lucas Grijó <rksgrijo@gmail.com>
2021-06-09 17:01:26 -0300

    Integração com os services. Porém atualmente está mostrando todas ao invez do usuário especifico.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,37 @@
+import { Ferias } from './../ferias';
+import { FeriasService } from './../ferias.service';
+import { Requerimento } from './../requerimento';
+import { RequerimentoService } from './../requerimento.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  requerimentos: Requerimento[] = [];
+  ferias: Ferias[] = [];
 
-  constructor() { }
+  constructor(
+    private requerimentoService: RequerimentoService,
+    private feriasService: FeriasService
+  ) {}
 
   ngOnInit(): void {
+    this.buscarTodosOsRequerimentos();
+    this.buscarTodasAsFerias();
   }
 
+  buscarTodosOsRequerimentos() {
+    this.requerimentoService
+      .buscarTodosOsRequerimentos()
+      .subscribe((requerimento) => (this.requerimentos = requerimento));
+  }
+
+  buscarTodasAsFerias() {
+    this.feriasService
+      .buscarTodasAsFerias()
+      .subscribe((ferias) => (this.ferias = ferias));
+  }
 }
